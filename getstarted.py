4170c29bc4f924e4bc42047e0be5fed85e90d41b
Fin M <fin@mace.law>
2025-07-12 22:51:33 -0400

    make the welcome into a panel and add a motto kinda thing

--- getstarted.py
+++ getstarted.py
@@ -1,5 +1,13 @@
 from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
 
 console = Console()
 
-console.print("[green]Welcome to HeyAI![/green]") # Welcome Message
+console.print(
+    Panel(
+        Text("Let's get started with your private \"Vibe Coding\" assistant.", justify="center"),
+        title="[green]Welcome to HeyAI![/green]",
+        expand=True,
+    )
+)
