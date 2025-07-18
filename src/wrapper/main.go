bed0966970605df35937ae08c62c57626e8f90cc
Fin M <fin@mace.law>
2025-07-18 11:08:15 -0400

    pretty much finish the wrapper

--- src/wrapper/main.go
+++ src/wrapper/main.go
@@ -4,10 +4,16 @@ package main
 
 import (
 	"os"
+	"os/exec"
+	"strings"
 )
+
+func get_backend() string {
+	return "heyai.py"
+}
 
 func main() {
     var args = os.Args[1:]
 
-
+	exec.Command(get_backend(), strings.Join(args, " "))
 }
