c175469743751321223959faef4472a51945be18
Fin M <fin@mace.law>
2025-07-18 10:57:48 -0400

    start creating build.py

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
 # Builds heyai to be ready for use
 
+import rich
+from rich.panel import Panel
+from rich.text import Text
+
+console = rich.get_console()
+
+console.print(Panel(
+    "[red]WARNING:[/red] This [blue]will[/blue] add to path.\nType [green]y[/green] to [blue]accept[/blue]."
+))
+
+assert input("> ") == "y", "Add to Path required"
+    
